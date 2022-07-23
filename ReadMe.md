3d3eeff5c9ad4e2385b3d88a93b42427187ade0b
antoponselvan <anto.ponselvan@gmail.com>
2022-07-23 23:38:44 +0800

    Rocks Moving + Able to Move Bird

--- ReadMe.md
+++ ReadMe.md
@@ -3,7 +3,6 @@
 Single player game in which the user navigates the bird thru multiple obstacles and at the same keep it afloat. The user will have options to launch straight missile to destroy obstacles
 
 ## User Story / Journey
-
 - User to get start button
 - User Can Move Bird - Up/Down to navigate Bird from obstacles
 - User to be able to launch missiles to destroy obstacles (Optional)
@@ -11,4 +10,20 @@ Single player game in which the user navigates the bird thru multiple obstacles 
 
 
 ## Wireframe
+Broad list of things to show:
+1. Start Screen - StartButton with a background just with the bird
+2. Game Play - Part with key Elements - Bird + Obstacle - Highlight key areas - Score board, manipulation button etc
+3. Game Play - Optional Features (Missile etc)
+4. Game End - Show the Score and Hide the key parts of the game (bird etc) that shows the game has really ended
+
+
+## Data
+Key Variables that defines the game state:
+1. Bird Position: x,y
+2. Total Score
+3. Obstacle Position: x, y
+4. Obstacle count
+
+
+
 
--- main.js
+++ main.js
@@ -2,40 +2,74 @@ import './style.css'
 import $ from "jquery";
 // sfb = Shooting Flappy Bird
 
-const sfb_up_delta = 0.01*$('#SFB').parent().height(); // sfb position Inc with Up arrow use
-const sfb_dn_delta = 0.01*$('#SFB').parent().height(); // sfb position Dec with Down arrow use
+// Main Variables defining game state
+const game = {};
+game.score = 0;
+game.birdAlive = true;
+game.sfb_upDelta = 1; // sfb position Inc with Up arrow use
+game.sfb_dnDelta = 1; // sfb position Dec with Down arrow use
+game.refreshRate = 100;
+game.sfb_pos = [25, 50];
+// game.rock_pos = [['0%', '65%'], ['20%', '90%']]
+game.rock_pos = [[90, 0], [75, 75]]
+game.first_render = true;
 
+// RENDER function (defining state of Game) ---------------------------
+const render = () => {
+  $('.rock').remove();
+  $('#scoreHolder').text("Score: "+ Math.round(game.score))
+  let rPos = game.rock_pos[0]
+  for (rPos of game.rock_pos){
+      const $div = $('<div>').addClass('rock');
+      $('.container_game').append($div);
+      $div.css({'left': ((rPos[0])+"%"), 'top':(rPos[1]+"%")})
+      console.log($('.container_game').css("height"))
+  }
+  $('#SFB').css({'left': ((game.sfb_pos[0])+"%"), 'top':(game.sfb_pos[1]+"%")})
+  console.log(game.first_render);
+  if (game.first_render) {
+    const timeStep = setInterval(time_step, game.refreshRate);
+    game.first_render = false;
+  }
+  if (!(game.birdAlive)) {clearInterval(timeStep)}
+
+}
+
+// Time Step Function ----------------------------------
+const time_step = () => {
+  let rPos = game.rock_pos[0]
+  game.rock_pos.forEach( (item, idx, arr) => {
+    arr[idx][0]-=1;
+    if (arr[idx][0] < 0){
+      arr.splice(idx,1)
+    }
+  })
+  game.score += 0.1
+  render();
+}
+
+// Up and Down Actions ----------------------------------------------
 const sfb_up = () => {
-  // alert($('#SFB').css)
-  // alert(sfb_up_delta);
-  // alert(($('#h1_1').height()));
-  // alert(parseInt($('.container_game').css("border-top-width")))
-  $('#SFB').css({top: ($('#SFB').position().top - sfb_up_delta - $('.container_game').position().top - parseInt($('.container_game').css("border-top-width")) )});
-  // alert($('#SFB').position().top)
+  if (game.sfb_pos[1]>0) {game.sfb_pos[1]-=1}
+  render();
 }
 const sfb_dn = () => {
-  $('#SFB').css({top: ($('#SFB').position().top + sfb_dn_delta - $('.container_game').position().top - parseInt($('.container_game').css("border-top-width")) )});
+  if (game.sfb_pos[1]<97) {game.sfb_pos[1]+=1}
+  render();
 }
-const main = () => {
-  
-  $('#SFB').css({'top': '50%', 'left':'25%'});
-  const $up_bttn = $('.button_UP');
-  $up_bttn.on('click', sfb_up);
-  const $dn_bttn = $('.button_DN');
-  $dn_bttn.on('click', sfb_dn);
-  // $('#SFB').position().top = 5000;
 
+
+// MAIN FUNCTION --------------------------------------------------
+const main = () => { 
+  $('#SFB').css({'top': '80%', 'left':'25%'});
+  $('.button_UP').on('click', sfb_up);
+  $('.button_DN').on('click', sfb_dn);
 
   $('body').on("keydown", (event) => {
-     if ((event.code === "ArrowUp")){
-      sfb_up();
-      console.log(event);
-    }
-    if ((event.code === "ArrowDown")){
-      sfb_dn();
-    }
-});
-
+     if ((event.code === "ArrowUp")){sfb_up()}
+    if ((event.code === "ArrowDown")){sfb_dn()}
+  })
+  render();
 }
 
 $(main);
