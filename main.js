019a93a41f3c075499efa5c12833b990e4f9b437
antoponselvan <anto.ponselvan@gmail.com>
2022-07-24 14:15:36 +0800

    Missile Feature Added

--- main.js
+++ main.js
@@ -4,22 +4,34 @@ import $ from "jquery";
 
 // Main Variables defining game state
 const game = {};
+
 game.score = 0;
-game.birdAlive = true;
+game.missileActive = false;
+game.birdAlive = false;
 game.sfb_upDelta = 1; // sfb position Inc with Up arrow use
 game.sfb_dnDelta = 1; // sfb position Dec with Down arrow use
+game.sfb_fallRate = 0.025 // sfb speed of natural fall
+game.missileMoveRate = 0.5; // Rock speed (%screen / 0.01s)
+game.genMissileTimePeriod = 500; // Total count of 0.01s before Rock Generation
+game.genMissileTimeCount = game.genMissileTimePeriod;
 game.rockMoveRate = 0.5; // Rock speed (%screen / 0.01s)
-game.genRockTimePeriod = 100; // Total count of 0.01s before Rock Generation
+game.genRockTimePeriod = 50; // Total count of 0.01s before Rock Generation
 game.genRockTimeCount = 0;
 game.sfb_pos = [25, 50];
 game.rock_pos = [[90, 10], [75, 75]]
+game.missile_posHt = [-1, -1, 0];
 
 
 // RENDER function (defining state of Game) ---------------------------
 const render = () => {
   $('.rock').remove();
-  if (game.birdAlive) {$('#scoreHolder').text("Score: "+ Math.round(game.score))}
-  else {$('#scoreHolder').text("Game Over! Final Score: "+ Math.round(game.score))}
+  if (game.birdAlive) {
+    $('#scoreHolder').text("Score: "+ Math.round(game.score))
+    $('#startStop').text("RESET")
+  } else {
+    $('#scoreHolder').text("Game Over! Final Score: "+ Math.round(game.score))
+    $('#startStop').text("START")
+  }
   let rPos = game.rock_pos[0]
   for (rPos of game.rock_pos){
       const $div = $('<div>').addClass('rock');
@@ -27,6 +39,7 @@ const render = () => {
       $div.css({'left': ((rPos[0])+"%"), 'top':(rPos[1]+"%")})
   }
   $('#SFB').css({'left': ((game.sfb_pos[0])+"%"), 'top':(game.sfb_pos[1]+"%")})
+  $('#missile').css({'left': ((game.missile_posHt[0])+"%"), 'top':(game.missile_posHt[1]+"%"), 'height':(game.missile_posHt[2]+"%")})
 }
 
 
@@ -42,37 +55,44 @@ const time_step = () => {
     }
   })
 
-  // Increment Score
-  game.score += 0.01
+  // Move Missile
+  if (game.missileActive){game.missile_posHt[0] += game.missileMoveRate;}
+  // if (game.genMissileTimeCount < game.genMissileTimePeriod) {game.genMissileTimeCount += 1;}
+  game.genMissileTimeCount += 1;
+  console.log(game.genMissileTimeCount);
 
-  // Check if Game Over
-  crashCheck();
-  
-  if (!(game.birdAlive)) {
-    clearInterval(game.timeStep);
-    gameOver();
-  }
 
   // Generate New Rocks
   game.genRockTimeCount +=1
   if (game.genRockTimeCount > game.genRockTimePeriod){
     game.genRockTimeCount = 0;
     let sfbPosFrac = (parseFloat($('#SFB').css('top'))/parseFloat($('.container_game').css('height')));
-    game.rock_pos.push([97, (100*sfbPosFrac*(1 + Math.random()*0.1))-10])
+    game.rock_pos.push([97, (100*sfbPosFrac*(1 + Math.random()*0.5))-10]);
   }
+
+  // Move SFB down
+  game.sfb_pos[1] +=game.sfb_fallRate;
+  game.sfb_fallRate *= 1.03 
+
+  // Increment Score
+  game.score += 0.01
+
+  // Check if Game Over or missile Hit
+  crashCheck();
+  missileHitCheck();  
+  if (!(game.birdAlive)) {clearInterval(game.timeStep)}
 
   complexityInc();
   render();
 }
 
-//Crash Check ------------------------------------------------
+//SFB Crash Check ------------------------------------------------
 const crashCheck = () => {
   let sfbTop = parseFloat($('#SFB').css("top"));
   let sfbLeft = parseFloat($('#SFB').css("left"));
   let sfbHeight = parseFloat($('#SFB').css("height"));
   let sfbWidth = parseFloat($('#SFB').css("width"));
   let containerHeight = parseFloat($('.container_game').css("height"));
-  console.log(containerHeight);
 
   for (let i in game.rock_pos){
     let rockTop = parseFloat($('.rock').eq(i).css("top"));
@@ -88,36 +108,62 @@ const crashCheck = () => {
 
     if (pt1Collide || pt2Collide || pt3Collide || pt4Collide || borderCollide) {
       game.birdAlive = false;
-      gameOver();
+      game.rock_pos = [[100,0]];
       return;
     }
   }
 }
 
+//Missile Hit Check ------------------------------------------------
+const missileHitCheck = () => {
+  let missileTop = parseFloat($('#missile').css("top"));
+  let missileLeft = parseFloat($('#missile').css("left"));
+  let missileHeight = parseFloat($('#missile').css("height"));
+  let missileWidth = parseFloat($('#missile').css("width"));
+  let containerWidth = parseFloat($('.container_game').css("width"));
+
+  for (let i in game.rock_pos){
+    let rockTop = parseFloat($('.rock').eq(i).css("top"));
+    let rockLeft = parseFloat($('.rock').eq(i).css("left"));
+    let rockHeight = parseFloat($('.rock').eq(i).css("height"));
+    let rockWidth = parseFloat($('.rock').eq(i).css("width"));
+
+    let pt1Collide = ((missileTop > rockTop) & (missileTop < (rockTop+rockHeight)) & (missileLeft > rockLeft) & (missileLeft < (rockLeft+rockWidth)))
+    let pt2Collide = (((missileTop+missileHeight) > rockTop) & ((missileTop+missileHeight) < (rockTop+rockHeight)) & (missileLeft > rockLeft) & (missileLeft < (rockLeft+rockWidth)))
+    let pt3Collide = ((missileTop > rockTop) & (missileTop < (rockTop+rockHeight)) & ((missileLeft+missileWidth) > rockLeft) & ((missileLeft+missileWidth) < (rockLeft+rockWidth)))
+    let pt4Collide = (((missileTop+missileHeight) > rockTop) & ((missileTop+missileHeight) < (rockTop+rockHeight)) & ((missileLeft+missileWidth) > rockLeft) & ((missileLeft+missileWidth) < (rockLeft+rockWidth)))    
+    let borderCollide = (missileLeft > 0.95*containerWidth);
+
+    if (pt1Collide || pt2Collide || pt3Collide || pt4Collide || borderCollide) {
+      game.missileActive = false;
+      game.missile_posHt = [-1, -1, 0];
+      game.rock_pos.splice(i,1);
+      return;
+    }
+  }
+}
 
 // Complexity Increase (Rock Speed & Gen Rate)-------------------
 const complexityInc = () => {
-  console.log("Complexity Inc");
   game.rockMoveRate += 0.0001;
-  game.genRockTimePeriod -= 0.01;
+  game.genRockTimePeriod -= 0.05;
 }
 
-// Game Over Display Handling ---------------------------------
-const gameOver = () => {
-  console.log("Game Over Display Change")
-  $('#startStop').text("START");
-  game.rock_pos = [[100,0]];
-  $('#scoreHolder').text(" GAME OVER!!   FINAL SCORE:"+game.score);
-}
-
-
-// Up and Down Actions ----------------------------------------------
+// User Actions (Up, Down, Missile-Launch) ------------------------------------------
 const sfb_up = () => {
   if (game.sfb_pos[1]>0) {game.sfb_pos[1]-=1}
+  game.sfb_fallRate = 0.025;
   render();
 }
 const sfb_dn = () => {
   if (game.sfb_pos[1]<97) {game.sfb_pos[1]+=1}
+  render();
+}
+const missile_launch = () => {
+  if ((game.missileActive === true)||(game.genMissileTimeCount < game.genMissileTimePeriod)) {return;}
+  game.missileActive = true;
+  game.missile_posHt = [(game.sfb_pos[0]+5), game.sfb_pos[1], 5];
+  game.genMissileTimeCount = 0;
   render();
 }
 
@@ -134,7 +180,6 @@ const Initialize = () => {
   game.genRockTimeCount = 0;
   game.sfb_pos = [25, 50];
   game.rock_pos = [[90, 10], [75, 75]]
-  $('#startStop').text("RESET")
   }
 
 
@@ -145,8 +190,9 @@ const main = () => {
   $('.button_DN').on('click', sfb_dn);
 
   $('body').on("keydown", (event) => {
-     if ((event.code === "ArrowUp")){sfb_up()}
+    if ((event.code === "ArrowUp")){sfb_up()}
     if ((event.code === "ArrowDown")){sfb_dn()}
+    if ((event.code === "ArrowRight")){missile_launch()}
   })
   
   $('#startStop').on("click", Initialize)
